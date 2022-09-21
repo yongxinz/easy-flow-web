c4a66d61896e786d038ae08e3a07261ada0d0850
yongxinz <hi_nature@yeah.net>
2022-09-21 17:45:37 +0800

    fix

--- src/views/system/settings/index.vue
+++ src/views/system/settings/index.vue
@@ -26,56 +26,10 @@
             <el-radio v-model="ruleForm.is_verify_code" :label="true">是</el-radio>
             <el-radio v-model="ruleForm.is_verify_code" :label="false">否</el-radio>
           </el-form-item>
-          <el-form-item label="LDAP登陆：">
-            <el-radio v-model="ruleForm.is_ldap" :label="true">是</el-radio>
-            <el-radio v-model="ruleForm.is_ldap" :label="false">否</el-radio>
-          </el-form-item>
           <el-form-item style="margin-bottom: 0">
             <el-button v-permisaction="['system:settings:index:config']" type="primary" @click="submitForm('ruleForm', 1)">确定</el-button>
           </el-form-item>
         </el-form>
-      </div>
-    </el-card>
-    <el-card class="box-card" style="margin-top: 15px">
-      <div slot="header" class="clearfix">
-        <span>Ldap配置</span>
-      </div>
-      <div class="text item">
-        <el-alert
-          title="Ldap登陆验证通过后，会将用户同步至本地数据库中，请在此配置数据同步的映射关系。单击Ldap字段可编辑，双击可隐藏编辑。"
-          type="info"
-          style="margin-bottom: 10px"
-        />
-        <el-table
-          :data="tableData"
-          border
-          style="width: 100%"
-        >
-          <el-table-column
-            prop="local_field_name"
-            label="字段名称"
-            width="150"
-          />
-          <el-table-column
-            prop="local_field_nick"
-            label="字段昵称"
-            width="150"
-          />
-          <el-table-column
-            prop="ldap_field_name"
-            label="Ldap字段"
-          >
-            <template slot-scope="{row, $index}">
-              <div style="width: 100%; min-height: 24px;" @click="{{ chengenum($index, true) }}" @dblclick="{{ chengenum($index, false) }}">
-                <el-input v-if="editable[$index]" v-model="row.ldap_field_name" />
-                <span v-else>{{ row.ldap_field_name }}</span>
-              </div>
-            </template>
-          </el-table-column>
-        </el-table>
-        <div style="margin-top: 20px">
-          <el-button v-permisaction="['system:settings:index:ldap']" type="primary" @click="submitForm('ruleForm', 2)">确定</el-button>
-        </div>
       </div>
     </el-card>
   </div>
@@ -120,13 +74,9 @@ export default {
             if (v.content === undefined || v.content === null) {
               this.ruleForm = {
                 name: '',
-                logo: '',
-                is_ldap: false
+                logo: ''
               }
             } else {
-              if (v.content.is_ldap === undefined || v.content.is_ldap === null) {
-                v.content.is_ldap = false
-              }
               this.ruleForm = v.content
             }
           } else if (v.classify === 2) {
