d052a9597b451ef0f9f1d9ede9c567d565863091
yongxinz <hi_nature@yeah.net>
2023-08-13 15:42:46 +0800

    feat: add workflow classify

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -1,6 +1,6 @@
 import variables from '@/styles/element-variables.scss'
 import defaultSettings from '@/settings'
-import { getSettings } from '@/api/system/settings'
+// import { getSettings } from '@/api/system/settings'
 
 const { title, logo, isLdap, showSettings, tagsView, fixedHeader, sidebarLogo, isVerifyCode } = defaultSettings
 
@@ -33,18 +33,18 @@ const mutations = {
 const actions = {
   changeSetting({ commit }, data) {
     commit('CHANGE_SETTING', data)
-  },
-  // get user info
-  getSystemSettings({ commit }) {
-    return new Promise((resolve) => {
-      getSettings({
-        classify: 1
-      }).then(response => {
-        commit('CHANGE_SYSTEM_SETTING', response.data[0].content)
-        resolve(response)
-      })
-    })
   }
+  // get user info
+//   getSystemSettings({ commit }) {
+//     return new Promise((resolve) => {
+//       getSettings({
+//         classify: 1
+//       }).then(response => {
+//         commit('CHANGE_SYSTEM_SETTING', response.data[0].content)
+//         resolve(response)
+//       })
+//     })
+//   }
 }
 
 export default {
